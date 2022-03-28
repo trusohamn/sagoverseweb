c2327950018e0b8a1c6a0dd69540ef63157d4bec
Marta Trusohamn <marta.trusohamn@gmail.com>
2022-03-28 21:09:11 +0200

    privacy policy

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,23 @@
-<script lang="ts">
-  export let name: string;
-</script>
-
-<main>
-  <h1>{name}</h1>
-  <p>Something magical is coming soon..</p>
-</main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
+<script>
+    import { Router, Route, Link } from "svelte-navigator";
+    import Home from "./views/Home.svelte";
+    import PrivacyPolicy from "./views/PrivacyPolicy.svelte"
+  
+  </script>
+  
+  <Router>
+    <main>  
+      <Route path="/">
+        <Home />
+      </Route>
+  
+      <Route path="privacy-policy">
+       <PrivacyPolicy/>
+      </Route>
+  
+      <Route>
+        <h3>Not found</h3>
+        <p>Page not found</p>
+      </Route>
+    </main>
+  </Router>
