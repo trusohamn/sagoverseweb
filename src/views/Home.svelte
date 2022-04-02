0f42394061cba608b9a0d8f9127c90876ded0f9a
Marta Trusohamn <marta.trusohamn@gmail.com>
2022-04-02 22:44:32 +0200

    basic home

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,23 +1,57 @@
-
+<script>
+  let src = "/assets/sagoverse-banner.jpeg";
+</script>
 
 <main>
-  <h1>Sagoverse</h1>
-  <p>Something magical is coming soon..</p>
+  <img {src} alt="Sagoverse banner" />
+  <buttonContainer>
+    <a href="https://testflight.apple.com/join/0jOyrOUT" class="button"
+      >Download test version for iOS</a
+    >
+    <a
+      href="https://play.google.com/store/apps/details?id=com.sagoverse"
+      class="button">Download test version for Android</a
+    >
+  </buttonContainer>
 </main>
 
 <style>
   main {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    background-color: #009d9d;
+  }
+  a.button {
+    padding: 8px;
+    margin: 5px;
+    border-radius: 5px;
+    appearance: button;
+    text-decoration: none;
+    color: initial;
+    background-color: rgba(192, 228, 217, 0.7);
     text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+    font-weight: 200;
+  }
+  buttonContainer {
+    position: absolute;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    top: 70%;
   }
 
   h1 {
     color: #ff3e00;
     text-transform: uppercase;
-    font-size: 4em;
+    font-size: 3em;
     font-weight: 100;
+  }
+
+  img {
+    max-height: 100%;
   }
 
   @media (min-width: 640px) {
