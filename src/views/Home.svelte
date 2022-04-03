b8c5ed357a61b7433ba0a5e9358a3c49984d6add
Marta Trusohamn <marta.trusohamn@gmail.com>
2022-04-03 21:45:14 +0200

    css

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -2,8 +2,10 @@
   let src = "/assets/sagoverse-banner.jpeg";
 </script>
 
-<main>
-  <img {src} alt="Sagoverse banner" />
+<container>
+  <div class="img-gradient">
+    <img {src} alt="Sagoverse banner" />
+  </div>
   <buttonContainer>
     <a href="https://testflight.apple.com/join/0jOyrOUT" class="button"
       >Download test version for iOS</a
@@ -13,24 +15,26 @@
       class="button">Download test version for Android</a
     >
   </buttonContainer>
-</main>
+</container>
 
 <style>
-  main {
+  container {
     height: 100%;
     width: 100%;
     display: flex;
     justify-content: center;
+    align-items: flex-start;
     background-color: #009d9d;
+    overflow: hidden;
   }
   a.button {
-    padding: 8px;
+    padding: 10px;
     margin: 5px;
     border-radius: 5px;
     appearance: button;
     text-decoration: none;
     color: initial;
-    background-color: rgba(192, 228, 217, 0.7);
+    background-color: rgba(192, 228, 217, 0.8);
     text-align: center;
     font-weight: 200;
   }
@@ -42,20 +46,36 @@
     width: 50%;
     top: 70%;
   }
+  .img-gradient {
+    top: 0;
+    width: 100%;
+    height: 80%;
+  }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 3em;
-    font-weight: 100;
+  .img-gradient::after {
+    display: block;
+    position: relative;
+    background-image: linear-gradient(
+      to bottom,
+      rgba(255, 255, 255, 0),
+      rgba(0, 157, 157, 0.6),
+      rgba(0, 157, 157, 0.9),
+      rgba(0, 157, 157, 0.95),
+      rgb(0, 157, 157)
+    );
+    margin-top: -50px;
+    height: 50px;
+    width: 100%;
+    content: "";
   }
 
   img {
-    max-height: 100%;
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
   }
-
   @media (min-width: 640px) {
-    main {
+    container {
       max-width: none;
     }
   }
