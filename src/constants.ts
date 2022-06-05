5efabcba6b6aa6530cf876e91387a477e0969b11
Marta Trusohamn <marta.trusohamn@gmail.com>
2022-06-05 23:15:13 +0200

    gave up with env

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,8 @@
-export const isDev = process.env.NODE_ENV === 'development';
+// export const isDev = process.env.NODE_ENV === 'development';
 
-export const service = isDev
+export const service = 'https://sagoverse.herokuapp.com'
+/* 
+isDev
     ? `http://${process.env.MY_IP}:${process.env.SERVER_PORT}`
     : 'https://sagoverse.herokuapp.com';
+ */
